ff40061c41182c821ada25a51d08c81a92369d60
Your Name <youremail@domain.com>
2023-05-26 18:39:29 +0530

    Add edit and delete funcanality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,17 +11,31 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     @song.save!
+    redirect_to songs_path
   end
 
   def edit
     @song = Song.find_by(id: params[:id].to_i) 
     @artists = Artist.all
+    
   end
   
   def update 
     @song = Song.find_by(id: params[:id].to_i) 
     @song.update(song_params)
+    redirect_to songs_path
   end
+
+  def show
+    @song = Song.find_by(id: params[:id].to_i)
+  end
+
+
+  def destroy
+    @song = Song.find_by(id: params[:id].to_i)
+    @song.destroy
+    redirect_to songs_path
+  end 
 
   private
     def song_params
